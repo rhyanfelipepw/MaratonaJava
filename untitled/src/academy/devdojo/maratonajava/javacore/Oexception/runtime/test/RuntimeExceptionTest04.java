68dcf4265656b1a84616c3a520c5647675ec0498
Rhyan Guarato <rhyanfelipeuniamerica@hotmail.com>
2023-03-03 08:59:10 -0300

    02/03

--- untitled/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ untitled/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -7,14 +7,10 @@ public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try{
             throw new IndexOutOfBoundsException();
-        }catch (ArrayIndexOutOfBoundsException e ){
+        }catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException exception ){
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
         }catch (IndexOutOfBoundsException e ){
             System.out.println("Dentro do IndexOutOfBoundsException");
-        }catch(IllegalArgumentException e){
-            System.out.println("Dentro do IllegalArgumentException");
-        }catch (ArithmeticException e){
-            System.out.println("Dentro do ArithmeticException");
         }catch (RuntimeException e){
             System.out.println("Dentro do RuntimeException");
         }
@@ -22,7 +18,7 @@ public class RuntimeExceptionTest04 {
 
         try {
             talvezLanceException();
-        } catch (Exception e) {
+        } catch (SQLException | FileNotFoundException e) {
            e.printStackTrace();
         }
     }
