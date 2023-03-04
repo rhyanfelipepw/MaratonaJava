b5c6e7ea17fa509a74263673ebd90cdcd14cb40a
Rhyan Guarato <rhyanfelipeuniamerica@hotmail.com>
2023-03-03 21:43:23 -0300

    03/03

--- untitled/src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ untitled/src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -1,11 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
+import java.time.LocalTime;
+
 public class Cliente {
 
     private String nome;
     private TipoCliente tipoCliente;
 
     private TipoPagamento tipoPagamento;
+
 
     public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
