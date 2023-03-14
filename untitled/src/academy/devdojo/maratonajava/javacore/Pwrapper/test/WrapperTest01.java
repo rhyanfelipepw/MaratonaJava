7f7c198eb0e96aac194ca0b4a063a8a63024f0be
Rhyan Guarato <rhyanfelipeuniamerica@hotmail.com>
2023-03-13 23:54:55 -0300

    13/03

---
+++ untitled/src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+
+        // PRIMITIVOS
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 1;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP= 'w';
+        boolean booleanP = false;
+
+
+        // WRAPPERS
+        Byte byteW= 1;
+        Short shortW= 1;
+        Integer intW= 1;
+        Long longW= 1L;
+        Float floatW= 10F;
+        Double doubleW= 10D;
+        Character charW = 'w';
+        Boolean booleanW= false;
+
+
+
+    }
+}
