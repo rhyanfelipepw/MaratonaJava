28257e39ee7c5e8efcead7e2336ad9942beda6cd
Rhyan Guarato <rhyanfelipeuniamerica@hotmail.com>
2023-03-29 21:52:57 -0300

    29/03

---
+++ untitled/src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTeste01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocalTeste01 {
+    public static void main(String[] args) {
+
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeCh = new Locale("it", "CH");
+        Locale localeIndia = new Locale("hi", "IN");
+        Locale localeJapao = new Locale("ja", "JP");
+        Locale localeHolanda = new Locale("nl", "NL");
+
+        Calendar ca = Calendar.getInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeIndia);
+        DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapao);
+        DateFormat df5 = DateFormat.getDateInstance(DateFormat.FULL, localeHolanda);
+
+        System.out.println("Italia:" + df1.format(ca.getTime()));
+        System.out.println("Suiça:" + df2.format(ca.getTime()));
+        System.out.println("India:" + df3.format(ca.getTime()));
+        System.out.println("Japão:" + df4.format(ca.getTime()));
+        System.out.println("Holanda:" + df5.format(ca.getTime()));
+
+        System.out.println(localeItaly.getDisplayCountry(localeJapao));
+        System.out.println(localeCh.getDisplayCountry(localeHolanda));
+    }
+}
